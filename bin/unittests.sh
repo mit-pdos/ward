43a232db14ed7bd7046c9db5c12141fe90c1fa08
Jonathan Behrens <fintelia@gmail.com>
2020-08-31 21:01:23 -0400

    Fixes

--- bin/unittests.sh
+++ bin/unittests.sh
@@ -10,11 +10,6 @@ anon 100000 || (echo FAILED && halt 1)
 echo OK
 echo
 
-echo Running schedbench...
-schedbench || (echo FAILED && halt 1)
-echo OK
-echo
-
 echo Running lebench...
 lebench - 20 || (echo FAILED && halt 1)
 echo OK
