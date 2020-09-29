05042478764667787bdb043faee701583da9d696
Jonathan Behrens <fintelia@gmail.com>
2020-09-29 18:47:52 -0400

    Re-enable benchbarriers

--- bin/benchbarriers.cc
+++ bin/benchbarriers.cc
@@ -1,56 +1,88 @@
-#include "user.h"
+#include "sysstubs.h"
 #include "stdio.h"
-#include "amd64.h"
 
-int main(int argc, int argv[]){
-  cmdline_change_param("lazy_barrier", "yes");
-  cmdline_change_param("kpti", "yes");
-  cmdline_change_param("track_wbs", "no");
+static inline uint64_t serialize_and_rdtsc() {
+  uint32_t cycles_low, cycles_high;
+  __asm volatile ("CPUID\n\t"
+                  "RDTSC\n\t"
+                  "mov %%edx, %0\n\t"
+                  "mov %%eax, %1\n\t"
+                  : "=r" (cycles_high), "=r" (cycles_low)
+                  :: "%rax", "%rbx", "%rcx", "%rdx");
+  return ((uint64_t)cycles_high << 32) | (uint64_t)cycles_low;
+}
+
+static inline uint64_t rdtscp_and_serialize() {
+  uint32_t cycles_low, cycles_high;
+  __asm volatile("RDTSCP\n\t"
+                 "mov %%edx, %0\n\t"
+                 "mov %%eax, %1\n\t"
+                 "CPUID\n\t"
+                 : "=r" (cycles_high), "=r" (cycles_low)
+                 :: "%rax", "%rbx", "%rcx", "%rdx");
+  return ((uint64_t)cycles_high << 32) | (uint64_t)cycles_low;
+}
+
+int main(int argc, char** argv){
+  ward_cmdline_change_param("keep_retpolines", "no");
+  ward_cmdline_change_param("lazy_barrier", "yes");
+  ward_cmdline_change_param("kpti", "yes");
+  ward_cmdline_change_param("track_wbs", "no");
 
   printf("Options, transparent (cycles), intentional (cycles)\n");
 
-  for(int style = 0; style < 4; style++) {
+  for(int style = 0; style < 5; style++) {
     switch(style) {
     case 0:
-      cmdline_change_param("spectre_v2", "no");
-      cmdline_change_param("mds", "no");
+      ward_cmdline_change_param("keep_retpolines", "no");
+      ward_cmdline_change_param("spectre_v2", "no");
+      ward_cmdline_change_param("mds", "no");
       printf("Neither, ");
       break;
     case 1:
-      cmdline_change_param("spectre_v2", "yes");
-      cmdline_change_param("mds", "no");
+      ward_cmdline_change_param("keep_retpolines", "no");
+      ward_cmdline_change_param("spectre_v2", "yes");
+      ward_cmdline_change_param("mds", "no");
       printf("SpectreV2, ");
       break;
     case 2:
-      cmdline_change_param("spectre_v2", "no");
-      cmdline_change_param("mds", "yes");
+      ward_cmdline_change_param("keep_retpolines", "no");
+      ward_cmdline_change_param("spectre_v2", "no");
+      ward_cmdline_change_param("mds", "yes");
       printf("MDS, ");
       break;
     case 3:
-      cmdline_change_param("spectre_v2", "yes");
-      cmdline_change_param("mds", "yes");
+      ward_cmdline_change_param("keep_retpolines", "no");
+      ward_cmdline_change_param("spectre_v2", "yes");
+      ward_cmdline_change_param("mds", "yes");
       printf("MDS+SpectreV2, ");
+      break;
+    case 4:
+      ward_cmdline_change_param("keep_retpolines", "yes");
+      ward_cmdline_change_param("spectre_v2", "yes");
+      ward_cmdline_change_param("mds", "yes");
+      printf("MDS+SpectreV2+QRetpoline, ");
       break;
     };
 
     int iters = 100000;
 
     for(int i = 0; i < iters/10; i++)
-      transparent_barrier(0);
+      ward_transparent_barrier(0);
 
     u64 start = serialize_and_rdtsc();
     for(int i= 0; i < iters; i++) {
-      transparent_barrier(0);
+      ward_transparent_barrier(0);
     }
     u64 end = rdtscp_and_serialize();
     printf("%ld, ", (end-start)/iters);
 
     for(int i = 0; i < iters/10; i++)
-      intentional_barrier(0);
+      ward_intentional_barrier(0);
 
     u64 start2 = serialize_and_rdtsc();
     for(int i= 0; i < iters; i++) {
-      intentional_barrier(0);
+      ward_intentional_barrier(0);
     }
     u64 end2 = rdtscp_and_serialize();
     printf("%ld\n", (end2-start2)/iters);
